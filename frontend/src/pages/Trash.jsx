d6ef5a671c40bc9c50b21460966756599d73381a
Sathish <nvssathish7309@gmail.com>
2026-08-24 17:29:23 +0530

    fix: resolve JSX map syntax error in Trash.jsx

--- frontend/src/pages/Trash.jsx
+++ frontend/src/pages/Trash.jsx
@@ -178,7 +178,7 @@ export default function Trash() {
                       </div>
                     </td>
                   </tr>
-                ))}
+                })}
               </tbody>
             </table>
           </div>
